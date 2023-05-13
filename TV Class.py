2ee8cef7ecd3025eb855378ccde571254d45dfdb
jayceee1207 <ablayjc7@gmail.com>
2023-05-13 17:41:49 +0800

    class TV: rename and fix arguments

--- TV Class.py
+++ TV Class.py
@@ -3,11 +3,12 @@
 #May 13, 2023
 
 #Create class named TV
-class Television:
-    def __init__(self, turn_on):
+class TV:
+    def __init__(self, turn_on, volume_level):
         self.turn_on = turn_on
+        self.volume_level = volume_level
 
 #Test Driver program TestTV
-television_1 = Television('On')
+television_1 = TV('On')
 
 print(television_1.turn_on)
