69c1ed62570aea0dc4d76c0aa079df6c2232b89a
jayceee1207 <ablayjc7@gmail.com>
2023-05-13 16:56:46 +0800

    Try class and self

--- TV Class.py
+++ TV Class.py
@@ -2,3 +2,10 @@
 #Test TV Class
 #May 13, 2023
 
+#Create class named TV
+class Television:
+    def __init__(self, turn_on, turn_off, get_channel, set_channel, 
+                get_volume, set_volume, channel_up, channel_down, volume_up, volume_down):
+        self.turn_on = bool
+        
+#Test Driver program TestTV
