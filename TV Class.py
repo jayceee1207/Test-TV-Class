60c02768cc45a41f0d9fe20d4f2b7f8cb4ac1c1e
jayceee1207 <ablayjc7@gmail.com>
2023-05-13 20:43:55 +0800

    <__main__.TV object at 0x0000022A0ED17450> = output

--- TV Class.py
+++ TV Class.py
@@ -4,23 +4,24 @@
 
 #Create class named TV
 class TV:
-    def __init__(self, get_channel, volume_level, turn_on, turn_off, set_channel, get_volume, set_volume,
-                 channel_up, channel_down, volume_up, volume_down):
-        self.get_channel = get_channel
-        self.volume_level = volume_level
-        self.turn_on = turn_on
-        self.turn_off = turn_off
+    def set_channel(self, set_channel):
         self.set_channel = set_channel
-        self.get_volume = get_volume
-        self.set_volume = set_volume
-        self.channel_up = channel_up
-        self.channel_down = channel_down
-        self.volume_up = volume_up
-        self.volume_down = volume_down
+    def set_volume_level(self, set_volume_level):
+        self.set_volume_level = set_volume_level
+    def show_channel(self):
+        return self.set_channel
+    def show_volume_level(self):
+        return self.show_volume_level
         
-#Test Driver program TestTV
-television_1 = TV(57, 5, True, False, 49, 6, 7, 4, 6,6,5) 
-print(television_1())
+television_1 = TV()
+television_1.set_channel(49)
+television_1.set_volume_level(7)
+television_1.show_channel()
+television_1.show_volume_level()
+
+
+        
+
 
 
 
