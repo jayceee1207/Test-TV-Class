140cd8f47238bf01c565be2d3171704f5654133d
jayceee1207 <ablayjc7@gmail.com>
2023-05-14 16:16:26 +0800

    Made a function for volume and channel

--- TV.py
+++ TV.py
@@ -2,6 +2,7 @@
 
 class TV:
 
+
     def __init__(self):
         self.channel = 1
         self.volume_level = 1
@@ -15,5 +16,22 @@ class TV:
     
     def get_tv_channel(self):
         return self.channel
+    
+    #This means that the current channel will be changed if new channel is within the range 1-120
+    def change_channel_tv (self, channel):
+        if 1 <= channel <= 120:
+            self.channel = channel
+
+    def get_tv_volume(self):
+        return self.get_tv_volume
+    
+    #This means that the current volume will be changed if new channel is within the range 1-7
+    def tv_volume_up(self, volume):
+        if 1 <= volume >= 7:
+            self.get_tv_volume = volume
+    
+
+
+
 
         
