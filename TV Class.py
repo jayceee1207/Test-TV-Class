ebfa1a053cb94ab383b07e1adfc77d06322155d5
jayceee1207 <ablayjc7@gmail.com>
2023-05-15 07:27:52 +0800

    There is no result in the terminal when I run the code

--- TV Class.py
+++ TV Class.py
@@ -14,6 +14,12 @@ class TVTest:
         self.tv1 = TV()
         self.tv2 = TV()
 
+        self.tv1.get_tv_channel(23)
+        self.tv1.get_tv_channel(5)
+
+        self.tv2.get_tv_channel(110)
+        self.tv2.get_tv_channel(7)
+
     def result(self):
         print ("tv1's channel is", self.tv1.get_tv_channel(), "and volume level is", self.tv1.get_tv_volume())
         print ("tv2's channel is", self.tv2.get_tv_channel(), "and volume level is", self.tv2.get_tv_volume())
