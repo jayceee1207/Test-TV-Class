eff61636265361e37d6654a497c294cb2aeae457
jayceee1207 <ablayjc7@gmail.com>
2023-05-15 07:24:49 +0800

    There is no code in the terminal when I run the code

--- TV Class.py
+++ TV Class.py
@@ -2,12 +2,21 @@
 #Test TV Class
 #May 13, 2023
 
-#Create class named TV
-from TV import add
 
-    
-sum = add(12,321)
-print(sum)
+
+from TV import TV
+
+
+class TVTest:
+
+    def __init__ (self):
+
+        self.tv1 = TV()
+        self.tv2 = TV()
+
+    def result(self):
+        print ("tv1's channel is", self.tv1.get_tv_channel(), "and volume level is", self.tv1.get_tv_volume())
+        print ("tv2's channel is", self.tv2.get_tv_channel(), "and volume level is", self.tv2.get_tv_volume())
 
 
 
