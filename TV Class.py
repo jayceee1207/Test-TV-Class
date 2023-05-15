863dee9508601bc8040e1a0760ecfa324c82dcfa
jayceee1207 <ablayjc7@gmail.com>
2023-05-15 21:05:19 +0800

    I remake the class and function

--- TV Class.py
+++ TV Class.py
@@ -12,16 +12,16 @@ def main():
     
     tv_1 = TV()
     tv_1.turn_on_tv()
-    tv_1.get_tv_channel()
-    tv_1.get_tv_volume()
+    tv_1.change_tv_channel(2,4)
+    tv_1.change_tv_volume(1)
 
     tv_2 = TV()
     tv_2.turn_on_tv()
-    tv_2.get_tv_channel()
-    tv_2.get_tv_volume()
+    tv_2.change_tv_channel(13,4)
+    tv_2.change_tv_volume(3)
 
-    print ("tv1's channel is", tv_1.get_tv_channel(), "and volume level is", tv_1.get_tv_volume())
-    print ("tv2's channel is", tv_2.get_tv_channel(), "and volume level is", tv_2.get_tv_volume())
+    print ("tv1's channel is", tv_1.change_tv_channel(), "and volume level is", tv_1.change_tv_volume())
+    print ("tv2's channel is", tv_2.change_tv_channel(), "and volume level is", tv_2.change_tv_volume())
 
 
     
--- TV.py
+++ TV.py
@@ -19,34 +19,36 @@ class TV:
     #This function returns the channel of TV.
     def get_tv_channel(self):
         return self.channel
-    
     #This means that the current channel will be changed if new channel is within the range 1-120
-    def change_channel_tv (self, channel):
-        if 1 <= channel <= 120:
+    def change_tv_channel (self, channel):
+        if self.turn_on_tv and 1 <= channel <= 120:
             self.channel = channel
 
     #This function increment the channel by one. If previous channel is 23, the new channel will be 24.
     def channel_tv_up(self, channel):
-        if 1 <= channel <= 120:
+        if self.turn_on_tv and 1 <= channel <= 120:
             self.channel += 1
     
     #This function decrement the channel by one. If previous channel is 23, the new channel will be 22.
     def channel_tv_down(self, channel):
-        if 1 <= channel <= 120:
+        if self.turn_on_tv and 1 <= channel <= 120:
             self.channel -= 1
 
     #This means that the current volume will be changed if new volume is within the range 1-7
     def get_tv_volume(self):
         return self.get_tv_volume
+    def change_tv_volume(self, volume_level):
+        if self.turn_on_tv and 1 <= volume_level <=7:
+            self.volume_level = volume_level
     
     #This function increment the channel by one. If previous volume is 3, the new channel will be 4.
     def tv_volume_up(self, volume):
-        if 1 <= volume >= 7:
+        if self.turn_on_tv and 1 <= volume >= 7:
             self.get_tv_volume += 1
     
     #This function decrement the channel by one. If previous volume is 3, the new channel will be 2.
     def tv_volume_down(self, volume):
-        if 1 <= volume >= 7:
+        if self.turn_on_tv and 1 <= volume >= 7:
             self.get_tv_volume -= 1
     
 
