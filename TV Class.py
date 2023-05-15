b225b77b44ae39339b4bd226ecc7853eaa679202
jayceee1207 <ablayjc7@gmail.com>
2023-05-15 18:19:33 +0800

    Identation

--- TV Class.py
+++ TV Class.py
@@ -7,22 +7,21 @@
 from TV import TV
 
 
-class TestTV:
 
-    def __init__ (self):
-        
-        tv_1 = TV()
-        tv_1.turn_on_tv()
-        tv_1.get_tv_channel(23)
-        tv_1.get_tv_channel(5)
+def main():
+    
+    tv_1 = TV()
+    tv_1.turn_on_tv()
+    tv_1.get_tv_channel(23)
+    tv_1.get_tv_channel(5)
 
-        tv_2 = TV()
-        tv_2.turn_on_tv()
-        tv_2.get_tv_channel(102)
-        tv_2.get_tv_channel(7)
+    tv_2 = TV()
+    tv_2.turn_on_tv()
+    tv_2.get_tv_channel(102)
+    tv_2.get_tv_channel(7)
 
-        print ("tv1's channel is", tv_1.get_tv_channel(), "and volume level is", tv_1.get_tv_volume())
-        print ("tv2's channel is", tv_2.get_tv_channel(), "and volume level is", tv_2.get_tv_volume())
+    print ("tv1's channel is", tv_1.get_tv_channel(), "and volume level is", tv_1.get_tv_volume())
+    print ("tv2's channel is", tv_2.get_tv_channel(), "and volume level is", tv_2.get_tv_volume())
 
 
     
