f06d8cee56c90d50a1ad60232c1c13b8f7cce03a
jayceee1207 <ablayjc7@gmail.com>
2023-05-16 07:17:13 +0800

    Made some revision: instead of listing 1-120 in an array, change it to range(120)

--- TV.py
+++ TV.py
@@ -7,13 +7,7 @@ class TV:
         #Inside a function are the objects. The default of channel and volume is 1 and the default boolean expression
         #is False that indicates that the TV is off.
         self.is_on = False
-        self.channel_list = [1,	2,	3,	4,	5,	6,	7,	8,	9, 10,  11,	12,	13,	14,	15,	16,	17,	18,	19, 20,
-                            21,	22,	23,	24,	25,	26,	27,	28,	29, 30,	31,	32,	33,	34,	35,	36,	37,	38,	39, 40,
-                            41,	42,	43,	44,	45,	46,	47,	48,	49, 50,	51,	52,	53,	54,	55,	56,	57,	58,	59, 60,
-                            61,	62,	63,	64,	65,	66,	67,	68,	69, 70,	71,	72,	73,	74,	75,	76,	77,	78,	79, 80,
-                            81,	82,	83,	84,	85,	86,	87,	88,	89, 90,	91,	92,	93,	94,	95,	96,	97,	98,	99, 100,
-                            101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
-                            117, 118, 119, 120]
+        self.channel_list = range(120)
         self.number_channel = len(self.channel_list)
         self.channel_index = 0
         self.volume_minimum = 0
