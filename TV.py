d0062df618bad55eb9e042d0e28f9adad26fbc92
jayceee1207 <ablayjc7@gmail.com>
2023-05-15 21:21:00 +0800

    fixed indentation and casing

--- TV.py
+++ TV.py
@@ -6,7 +6,7 @@ class TV:
     def __init__(self):
         #Inside a function are the objects. The default of channel and volume is 1 and the default boolean expression
         #is False that indicates that the TV is off.
-        self.isOn = False
+        self.is_on = False
         self.channel_list = [1,	2,	3,	4,	5,	6,	7,	8,	9, 10,  11,	12,	13,	14,	15,	16,	17,	18,	19, 20,
                             21,	22,	23,	24,	25,	26,	27,	28,	29, 30,	31,	32,	33,	34,	35,	36,	37,	38,	39, 40,
                             41,	42,	43,	44,	45,	46,	47,	48,	49, 50,	51,	52,	53,	54,	55,	56,	57,	58,	59, 60,
@@ -22,7 +22,7 @@ class TV:
 
     #This function returns a boolean expression True if the TV is on.
     def turn_on(self):
-        self.isOn = not self.isOn
+        self.is_on = not self.is_on
     
     #This function returns a boolean expression False if the TV is on.
     def turn_off_tv(self):
@@ -31,13 +31,13 @@ class TV:
     #This function returns the channel of TV.
 
     #This means that the current channel will be changed if new channel is within the range 1-120
-    def setChannel(self, new_channel):
+    def set_channel(self, new_channel):
         if new_channel in self.channel_list:
             self.channel_index = self.channel_list.index(new_channel)
 
     #This function increment the channel by one. If previous channel is 23, the new channel will be 24.
     def channel_up(self):
-        if not self.isOn:
+        if not self.is_on:
             return
         self.channel_index += 1
         if self.channel_index > self.number_channel:
@@ -45,7 +45,7 @@ class TV:
     
     #This function decrement the channel by one. If previous channel is 23, the new channel will be 22.
     def channel_up(self):
-        if not self.isOn:
+        if not self.is_on:
             return
         self.channel_index -= 1
         if self.channel_index < self.number_channel:
@@ -56,19 +56,21 @@ class TV:
     
     #This function increment the channel by one. If previous volume is 3, the new channel will be 4.
     def volume_up(self):
-        if not self.isOn:
+        if not self.is_on:
             return
         if self.volume < self.volume_maximum:
             self.volume += 1
     
     #This function decrement the channel by one. If previous volume is 3, the new channel will be 2.
     def volume_down(self):
-        if not self.isOn:
+        if not self.is_on:
             return
         if self.volume > self.volume_maximum:
             self.volume -= 1
 
-
+    def display_result(self):
+            print("TV's channel is", self.channel_list[self.channel_index], 
+            "and volume level is", self.volume)
 
 
 
